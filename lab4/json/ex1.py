5677d40bb1dda64568c19647a40dc79a94ceabb1
pp2-22B030163 <k_piltan@kbtu.kz>
2023-02-18 21:17:41 +0600

    LAB4

---
+++ lab4/json/ex1.py
@@ -0,0 +1,20 @@
+import json
+
+file = open('01.json', 'r')
+js = json.load(file)
+
+
+
+print("Interface Status\n================================================================================")
+print("DN                                                 Description           Speed    MTU  ")
+print("-------------------------------------------------- --------------------  ------  ------")
+
+for i in js['imdata']:
+    print(i['l1PhysIf']['attributes']['dn'], end ="                       \t")
+    print(i['l1PhysIf']['attributes']['descr'], end= "  \t")
+    print( i['l1PhysIf']['attributes']['speed'], end= "   ")
+    print( i['l1PhysIf']['attributes']['mtu'], end= '\n')
+
+
+
+
